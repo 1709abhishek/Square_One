9757a3f22759e13d2cad7f46ed1c453bad93646b
Abhishek Jain <1709abhishek@gmail.com>
2020-04-17 13:03:41 +0530

    passport auth complete

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo")(session);
+
+app.use(express.urlencoded());
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    name: "squareOne",
+    // TODO change the secret before deployment in production mode
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongo all set");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use("/", require("./routes/api/v1"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
+});
